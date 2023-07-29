f9891c022f7da9bc715a7068ebe5d891c7a2fa19
cReAtOR0009 <epidnugoairdrop@gmail.com>
2023-07-29 01:13:27 +0100

    first deploy

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOzigBbrGzswkfZ0EBQCGYXZWaoDDZFhM",
+  authDomain: "disney-clone-a1c80.firebaseapp.com",
+  projectId: "disney-clone-a1c80",
+  storageBucket: "disney-clone-a1c80.appspot.com",
+  messagingSenderId: "53256443607",
+  appId: "1:53256443607:web:bb890441fb9485ee004292",
+  measurementId: "G-PY4BMERX40",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
